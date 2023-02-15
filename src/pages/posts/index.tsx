674a92e3b38f66d67bd4dd9b79e6df7db684d47d
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2023-02-15 13:18:23 +0700

    getStaticProps

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,29 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
 
-export interface PostsPageProps {
+export interface PostsListPageProps {
+  posts: any[],
 }
 
-export default function PostsPage (props: PostsPageProps) {
+export default function PostsListPage ( {posts}: PostsListPageProps) {
   return (
     <div>
-      Posts Page
+      <h1>Posts List Page</h1>
+      <ul>
+        { posts.map(post => <li key={post.id}>{post.title}</li>) }
+      </ul>
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async(context: GetStaticPropsContext) => {
+  console.log('get static props')
+  const response = await fetch('http://localhost:4000/post?page=1');
+  const data = await response.json()
+  console.log(data);
+  return {
+    props: {
+      posts:  data.map( (x: any) => ({id: x._id, title: x.title})),
+    }
+  }
 }
