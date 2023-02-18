c7273086cd33e0edc85cfe878c73185e0817d44d
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2023-02-18 23:03:24 +0700

    getServerSideProps

---
+++ src/pages/posts/params.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+
+export interface CatchAllParamsPageProps {}
+
+export default function CatchAllParamsPage(props: CatchAllParamsPageProps) {
+  const router = useRouter()
+  const { params } = router.query
+  return (
+    <div>
+      <h1>Catch All Params Page</h1>
+
+      <p>Params: {JSON.stringify(router.query)}</p>
+      <p>First param: {Array.isArray(params) ? params[0] : ''}</p>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  return {
+    props: {},
+  }
+}
