945dc80a625bd8b9e35ebd2a8ea6a6bc5b0bcabc
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2023-02-20 12:51:31 +0700

    03-02: create api

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data =
+  | {
+      id: string
+      title?: string
+      content?: string
+    }
+  | { name: string }
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'GET') {
+    return res.status(404).json({ name: 'method not supported' })
+  }
+
+  const resposne = await fetch('http://localhost:4000/post?page=1&limit=10')
+  const data = await resposne.json()
+  res.status(200).json(data)
+}
