9b47441fbe17411a417f4b9274ff97acafb4b093
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2023-03-03 11:25:47 +0700

    Responsive Hero Section

--- src/components/common/home/hero.tsx
+++ src/components/common/home/hero.tsx
@@ -1,20 +1,27 @@
-import { Box, Button, Container, Stack, Typography } from '@mui/material'
+import { Button, Container, Stack, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import Image from 'next/image'
 import * as React from 'react'
 import avatar from '@/images/Ellipse 1.png'
+import { relative } from 'path'
 
 export function HeroSection() {
   return (
-    <Box component="section" pt={18} pb={9}>
+    <Box component="section" pt={{ xs: 4, md: 16 }} pb={{ xs: 7, md: 9 }}>
       <Container>
-        <Stack direction="row" alignItems="flex-start" spacing={5}>
+        <Stack
+          direction={{ xs: 'column-reverse', md: 'row' }}
+          alignItems={{ xs: 'center', md: 'flex-start' }}
+          spacing={8}
+          textAlign={{ xs: 'center', md: 'left' }}
+        >
           <Box>
-            <Typography component="h1" variant="h3" fontWeight="bold" mb={5}>
+            <Typography component="h1" variant="h3" fontWeight="bold" mb={{ xs: 3.5, md: 5 }}>
               Hi, I am John,
               <br />
               Creative Technologist
             </Typography>
-            <Typography mb={5}>
+            <Typography mb={{ xs: 3.5, md: 5 }}>
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
               officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
               amet.
@@ -25,13 +32,16 @@ export function HeroSection() {
           </Box>
           <Box
             sx={{
-              minWidth: '240px',
+              //   minWidth: '240px',
               boxShadow: '-5px 13px',
               color: 'secondary.light',
+              bgcolor: 'primary.light',
               borderRadius: '50%',
+              width: '243px',
+              height: '243px',
             }}
           >
-            <Image src={avatar} alt="avatar" />
+            <Image src={avatar} alt="avatar" width={243} height={243} />
           </Box>
         </Stack>
       </Container>
